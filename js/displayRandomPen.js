db88109585ac0bf7bdee8de4cd660ae35312108b
lisihocke <lisihocke@users.noreply.github.com>
2021-02-03 01:24:32 +0100

    Display random CodePens on request

--- js/displayRandomPen.js
+++ js/displayRandomPen.js
@@ -1,2 +1,16 @@
-var random = Math.floor(Math.random() * document.querySelectorAll('.embeddedPen').length);
-document.querySelectorAll('.embeddedPen')[random].style.display = 'block';
+function displayRandomPen() {
+    var embeddedPens = document.querySelectorAll('.embeddedPen');
+    hideElements(embeddedPens);
+    showRandomElement(embeddedPens);
+}
+
+function hideElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'none';
+    }
+}
+
+function showRandomElement(elements) {
+    var randomElement = Math.floor(Math.random() * elements.length);
+    elements[randomElement].style.display = 'block';
+}
